9bbb1a8d2ffd7be46de334be510b0a6655065040
Matthew Seidel <matt@Matthews-MacBook.local>
2022-03-18 09:25:28 -0500

    added profiles. styled profiles nearly complete.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import StudentCard from "./studentCard/studentCard.component";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <StudentCard></StudentCard>
+    </div>
+  );
+}
+
+export default App;
